7fade4720a8f601424bc1e9649b418f128fb4b6a
AdrianoSSS <143419181+AdrianoSSS@users.noreply.github.com>
2024-10-31 12:05:05 +0000

    oi

---
+++ colab-2.py
@@ -0,0 +1,19 @@
+import os 
+os.system ("cls||clear")
+print ("Adriano")
+
+#COMANDOS DO GIT
+#Adiciona o repositorio atual com dado do repositorio no GitHub
+#git pull
+
+#ADICIONA NOVOS DADOS PRA O COMMIT
+#git add.
+
+#VERISONA COM OS NOVOS DADOS
+#git commit -m "OI"
+
+#ATUALIZA TRAZENDO COMMITS DO GITHUB
+#git pull
+
+#ENVIA OS COMMITS PARA O GITHUB
+#git push
